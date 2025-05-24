26d0e79cbe9a6e5491e973cf48be0cf893f40b19
shreesha.acharya@sap.com <shreesha.acharya@sap.com>
2025-05-25 04:33:32 +0530

    database key added

--- student_form_app/settings.py
+++ student_form_app/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'student_form_app.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 AUTH_PASSWORD_VALIDATORS = []
