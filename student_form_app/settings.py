d29f03ca07b12a2ba7ee8581c640e1024adbf0dd
shreesha.acharya@sap.com <shreesha.acharya@sap.com>
2025-05-25 04:02:17 +0530

    allowed host added

--- student_form_app/settings.py
+++ student_form_app/settings.py
@@ -9,7 +9,8 @@ SECRET_KEY = 'django-insecure-12345'
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['student_form_app.onrender.com']
+ALLOWED_HOSTS = ['akbms.onrender.com']
+# ALLOWED_HOSTS = ['student_form_app.onrender.com']
 
 INSTALLED_APPS = [
     
@@ -63,15 +64,15 @@ WSGI_APPLICATION = 'student_form_app.wsgi.application'
 #         'PORT': os.environ.get('DB_PORT', '5432'),
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600)
+# }
 
 AUTH_PASSWORD_VALIDATORS = []
 
